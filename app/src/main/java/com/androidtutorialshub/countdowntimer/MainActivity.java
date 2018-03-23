c733fdfde3c01d6d17ce4def9632ab33c9617ecc
Hector Speight <support@heandfi.org>
2018-03-23 02:10:08 +0000

    Landscape image resize

--- app/src/main/java/com/androidtutorialshub/countdowntimer/MainActivity.java
+++ app/src/main/java/com/androidtutorialshub/countdowntimer/MainActivity.java
@@ -1,19 +1,32 @@
 package com.androidtutorialshub.countdowntimer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
-import android.widget.EditText;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public String DEBUG_TAG = "MA";
+
+    DecimalFormat formatter = new DecimalFormat("#,###,###");
 
     private long timeCountInMilliSeconds = 1 * 60000;
 
@@ -24,12 +37,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TimerStatus timerStatus = TimerStatus.STOPPED;
 
-    private ProgressBar progressBarCircle;
-    private EditText editTextMinute;
-    private TextView textViewTime;
-    private ImageView imageViewReset;
-    private ImageView imageViewStartStop;
+    private ProgressBar progressBarCircleSec;
+    private ProgressBar progressBarCircleMin;
+    private ProgressBar progressBarCircleHrs;
+    private ProgressBar progressBarCircleDay;
+    private TextView textViewSec;
+    private TextView textViewMin;
+    private TextView textViewHrs;
+    private TextView textViewDay;
+    private TextView textViewYrs;
+    private TextView textViewDayLit;
+    private TextView textViewYrsLit;
     private CountDownTimer countDownTimer;
+    private boolean togDays = true;
+    private String orientation;
 
 
     @Override
@@ -42,26 +63,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // method call to initialize the listeners
         initListeners();
 
-
     }
 
     /**
      * method to initialize the views
      */
     private void initViews() {
-        progressBarCircle = (ProgressBar) findViewById(R.id.progressBarCircle);
-        editTextMinute = (EditText) findViewById(R.id.editTextMinute);
-        textViewTime = (TextView) findViewById(R.id.textViewTime);
-        imageViewReset = (ImageView) findViewById(R.id.imageViewReset);
-        imageViewStartStop = (ImageView) findViewById(R.id.imageViewStartStop);
+
+        progressBarCircleSec = findViewById(R.id.progressBarCircleSec);
+        progressBarCircleMin = findViewById(R.id.progressBarCircleMin);
+        progressBarCircleHrs = findViewById(R.id.progressBarCircleHrs);
+        progressBarCircleDay = findViewById(R.id.progressBarCircleDay);
+        textViewSec = findViewById(R.id.textViewSec);
+        textViewMin = findViewById(R.id.textViewMin);
+        textViewHrs = findViewById(R.id.textViewHrs);
+        textViewDay = findViewById(R.id.textViewDay);
+        textViewDayLit = findViewById(R.id.textViewDayLit);
+        textViewYrsLit = findViewById(R.id.textViewYrsLit);
+        textViewDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                togDays = !togDays; // toggle value
+                 //Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        textViewYrs = findViewById(R.id.textViewYrs);
+        if (textViewYrs != null) {
+            //imageRotatorFragment.setImageSelected(imageItem, position);
+            //Log.d(DEBUG_TAG, "LANDSCAPE");
+            orientation = "LANDSCAPE";
+            textViewYrs = findViewById(R.id.textViewYrs);
+        } else {
+            //Log.d(DEBUG_TAG, "PORTRAIT");
+            orientation = "PORTRAIT";
+        }
+
     }
 
     /**
      * method to initialize the click listeners
      */
     private void initListeners() {
-        imageViewReset.setOnClickListener(this);
-        imageViewStartStop.setOnClickListener(this);
+        //imageViewReset.setOnClickListener(this);
+        //imageViewStartStop.setOnClickListener(this);
     }
 
     /**
@@ -72,12 +116,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.imageViewReset:
-                reset();
-                break;
-            case R.id.imageViewStartStop:
-                startStop();
-                break;
+           // case R.id.imageViewStartStop:
+            //    startStop();
+             //   break;
         }
     }
 
@@ -94,36 +135,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * method to start and stop count down timer
      */
     private void startStop() {
-        if (timerStatus == TimerStatus.STOPPED) {
 
             // call to initialize the timer values
             setTimerValues();
             // call to initialize the progress bar values
-            setProgressBarValues();
-            // showing the reset icon
-            imageViewReset.setVisibility(View.VISIBLE);
-            // changing play icon to stop icon
-            imageViewStartStop.setImageResource(R.drawable.icon_stop);
-            // making edit text not editable
-            editTextMinute.setEnabled(false);
-            // changing the timer status to started
-            timerStatus = TimerStatus.STARTED;
-            // call to start the count down timer
+            //setProgressBarValues();
             startCountDownTimer();
-
-        } else {
-
-            // hiding the reset icon
-            imageViewReset.setVisibility(View.GONE);
-            // changing stop icon to start icon
-            imageViewStartStop.setImageResource(R.drawable.icon_start);
-            // making edit text editable
-            editTextMinute.setEnabled(true);
-            // changing the timer status to stopped
-            timerStatus = TimerStatus.STOPPED;
-            stopCountDownTimer();
-
-        }
 
     }
 
@@ -131,16 +148,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * method to initialize the values for count down timer
      */
     private void setTimerValues() {
-        int time = 0;
-        if (!editTextMinute.getText().toString().isEmpty()) {
-            // fetching value from edit text and type cast to integer
-            time = Integer.parseInt(editTextMinute.getText().toString().trim());
-        } else {
-            // toast message to fill edit text
-            Toast.makeText(getApplicationContext(), getString(R.string.message_minutes), Toast.LENGTH_LONG).show();
-        }
+        //int time = 1;
         // assigning values after converting to milliseconds
-        timeCountInMilliSeconds = time * 60 * 1000;
+        // timeCountInMilliSeconds = time * 60 * 1000;
+        //timeCountInMilliSeconds = 10000;
     }
 
     /**
@@ -148,28 +159,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void startCountDownTimer() {
 
-        countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
+        int mTime = 1518926400; // 11/2/18 04:00:00
+        //int mTime = -314373600; // my DOB
+        //int mTime = 1204744736; // 5/3/08 19:18:56
+        //int mTime = 749383200; // 30/9/93 10:00:00
+        final long millisToStart = 86500000;                //86400000 = milliseconds in 1 day
+        long td = System.currentTimeMillis() / 1000;
+        final long timeDiff = td - mTime ;
+
+        countDownTimer = new CountDownTimer(millisToStart, 1000) {
+            //final long timeDiff = td - mTime ;
+
             @Override
             public void onTick(long millisUntilFinished) {
 
-                textViewTime.setText(hmsTimeFormatter(millisUntilFinished));
+                long secs = timeDiff + ((millisToStart / 1000) - ((int) (millisUntilFinished / 1000)));
+                long modSecs = secs % 60;
+                long minsInHour = (secs / 60) % 60;
+                //long minsInTotal = secs / 60;
+                long daysInTotal = TimeUnit.SECONDS.toDays(secs);
+                long hours = TimeUnit.SECONDS.toHours(secs) % 24;
+                long hrsInTotal = secs / 3600;
+                int days = (int) TimeUnit.SECONDS.toDays(secs);
+                //Log.d(DEBUG_TAG, formatter.format(days));
+                int modDays = (int) Math.floor(days % 365);
+                double years = days / 365.25;
+                int weeks = days / 7;
 
-                progressBarCircle.setProgress((int) (millisUntilFinished / 1000));
+                //Log.d(DEBUG_TAG, formatter.format(minsInTotal));
+                textViewSec.setText(String.valueOf(modSecs));
+                textViewMin.setText(formatter.format(minsInHour)); //probably need to not format if more than 7 digits
+                textViewHrs.setText(formatter.format(hours));
+                textViewDay.setText(formatter.format(modDays));
+
+                if (orientation == "LANDSCAPE") {
+                    if (togDays) {
+                        textViewYrs.setText(formatter.format(weeks));
+                        textViewYrsLit.setText("Weeks");
+                    } else {
+                        textViewYrs.setText(String.valueOf((int) Math.floor(years)));
+                        textViewYrsLit.setText("Years");
+                    }
+                } else {
+                    // Display days or weeks depending on status of toggle
+                    if (togDays) {
+                        textViewDay.setText(formatter.format(days));
+                        textViewDayLit.setText("Days");
+                    } else {
+                        textViewDay.setText(formatter.format(weeks));
+                        textViewDayLit.setText("Weeks");
+                    }
+                }
+                progressBarCircleSec.setProgress((int) (modSecs));
+                progressBarCircleMin.setProgress((int) (minsInHour));
+                //Log.d(DEBUG_TAG, formatter.format(minsInHour));
+                progressBarCircleHrs.setProgress((int) (hours));
+
+                //progressBarCircle.setProgress((int) (hrsInDay)); //
 
             }
 
             @Override
             public void onFinish() {
 
-                textViewTime.setText(hmsTimeFormatter(timeCountInMilliSeconds));
+                textViewSec.setText(hmsTimeFormatter(timeCountInMilliSeconds));
                 // call to initialize the progress bar values
                 setProgressBarValues();
                 // hiding the reset icon
-                imageViewReset.setVisibility(View.GONE);
                 // changing stop icon to start icon
-                imageViewStartStop.setImageResource(R.drawable.icon_start);
+                //imageViewStartStop.setImageResource(R.drawable.icon_start);
                 // making edit text editable
-                editTextMinute.setEnabled(true);
+                //editTextMinute.setEnabled(true);
                 // changing the timer status to stopped
                 timerStatus = TimerStatus.STOPPED;
             }
@@ -190,8 +250,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void setProgressBarValues() {
 
-        progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
-        progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+        //progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
+        //progressBarCircleSec.setMax(60);
+        //progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+        //progressBarCircleSec.setProgress(60);
+        // don't think it makes sense to have a max & progress for cascaded values when counting up
     }
 
 
@@ -203,9 +266,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private String hmsTimeFormatter(long milliSeconds) {
 
-        String hms = String.format("%02d:%02d:%02d",
-                TimeUnit.MILLISECONDS.toHours(milliSeconds),
-                TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
+        String hms = String.format(Locale.getDefault(),"%02d",
+                //TimeUnit.MILLISECONDS.toHours(milliSeconds),
+                //TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
                 TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)));
 
         return hms;
@@ -213,5 +276,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+/*
+    private static String getScreenResolution(Context context) {
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+       // Toast.makeText(context, "{" + width + "," + height + "}", Toast.LENGTH_LONG).show();
+        return "{" + width + "," + height + "}";
+    }
+*/
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startStop();
+    }
 
 }
